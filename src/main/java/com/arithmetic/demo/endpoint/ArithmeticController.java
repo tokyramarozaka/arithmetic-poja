44dcc009534c7ee0f446c9789340f0eab6ca32a6
tokyramarozaka <tokyramarozaka@gmail.com>
2025-06-24 08:47:21 +0300

    feat: add add operation with test

---
+++ src/main/java/com/arithmetic/demo/endpoint/ArithmeticController.java
@@ -0,0 +1,14 @@
+package com.arithmetic.demo.endpoint;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArithmeticController {
+
+    @GetMapping("/add")
+    public long add(@RequestParam long a, @RequestParam long b) {
+        return a + b;
+    }
+}
